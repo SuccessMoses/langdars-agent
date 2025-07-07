c988322ee123becb2a0eb424baf8707b36e2655d
Success Moses <syko2021@gmail.com>
2025-07-07 18:49:54 +0100

    Jul 7 18:49

--- src/ipython_container/base_env.py
+++ src/ipython_container/base_env.py
@@ -343,16 +343,16 @@ class BaseSWEEnv(gym.Env):
             )
 
         assert self.container_obj is not None # Ensure container_obj is initialized
-        self.container_obj.copy_file_to_container(
+        self.container_obj.write_file_to_container(
             host_src_path=os.path.join(os.getcwd(), "src", "utils", "utils_codegraph.py"),
             container_dest_path="/root/utils_codegraph.py"
         )
-        self.container_obj.copy_file_to_container(
+        self.container_obj.write_file_to_container(
             host_src_path=os.path.join(os.getcwd(), "src", "utils", "construct_graph.py"),
             container_dest_path="/root/construct_graph.py"
         )
         # Move graph retrieval script to the container
-        self.container_obj.copy_file_to_container(
+        self.container_obj.write_file_to_container(
             host_src_path=os.path.join(os.getcwd(), "src", "utils", "retrieve_graph.py"),
             container_dest_path="/root/retrieve_graph.py"
         )
